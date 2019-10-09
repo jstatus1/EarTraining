d5c1374b23dc5a29a742a4065e5bf275b0c458b0
jstatus1 <36826890+jstatus1@users.noreply.github.com>
2019-10-08 23:57:39 -0500

    update

---
+++ Assets/Scripts/Managers/QuizManager.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+/*
+    * Purpose: Stores all the options that the user wants for his quiz
+ */
+namespace Managers
+{
+    public class QuizManager : MonoBehaviour
+    {   
+
+        private static QuizManager _instance;
+        public static QuizManager Instance{get{return _instance;}}
+
+        [Header("User Desired Quiz Training")]
+        public List<IntervalData> IntervalList;
+        public int NumOfChoices;
+
+        void Awake()
+        {
+            if(_instance != null && _instance != this)
+            {
+                Destroy(this.gameObject);
+            }else{
+                _instance = this;
+            }
+            DontDestroyOnLoad(this);
+        }
+
+
+
+    }
+
+}
+    
---
+++ Assets/Scripts/UI/QuizSettingsListener.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+namespace UI.QuizSetting
+{
+    public class QuizSettingsListener : MonoBehaviour
+    {
+          [Header("Button Configuration")]
+          [SerializeField] Button Button_Play;
+          [SerializeField] Button Button_Back;
+          [SerializeField] Button Button_Forward;
+          
+
+          [Header("Stored Settings")]
+          [SerializeField] List<IntervalData> List_Data;
+          [SerializeField] TMP_Dropdown DropDown_Choices;
+
+          [Header("Question Configuration")]
+          [SerializeField] List<GameObject> List_Questions;
+
+        
+
+        
+    }
+}
+
