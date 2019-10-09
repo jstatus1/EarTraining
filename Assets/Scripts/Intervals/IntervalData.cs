e4d573fd69747f83a15cbdfdac17a1cc5f957778
jstatus1 <36826890+jstatus1@users.noreply.github.com>
2019-10-08 22:41:59 -0500

    Update

---
+++ Assets/Scripts/Intervals/IntervalData.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+[CreateAssetMenu(fileName = "Interval_Data", menuName= "ScriptableObjects/IntervalData")]
+public class IntervalData : ScriptableObject
+{
+    public string Title_Interval;
+    public AudioClip AudioClip_Interval;
+    public List<AudioClip> AudioClip_ListInterval;
+    public string Information_Interval;
+    public Image Image_Interval;
+
+
+}
