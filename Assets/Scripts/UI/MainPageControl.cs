8fc7cb300fbcd5de01d0813e78fd82516e84be70
Johnson Tran <johnsontothetran1@gmail.com>
2019-10-20 01:24:24 -0500

    Update

---
+++ Assets/Scripts/UI/MainPageControl.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+///<summary>
+/// main controls for the front screen
+////</summary>
+public class MainPageControl : MonoBehaviour
+{
+    [SerializeField] Button Button_EarTraining;
+    // Start is called before the first frame update
+    void Start()
+    {
+        SetButton();
+    }
+
+
+    void SetButton()
+    {
+        Button_EarTraining.onClick.AddListener(() => {
+            SceneManager.LoadScene("3_PreConfiguration_EarTraining");
+        });
+    }
+}
